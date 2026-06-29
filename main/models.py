7a986bc6a8ff771bca235367e5f0284e1ae57da6
javoxiroritqov1 <javoxiroritqov14@gmail.com>
2026-06-30 03:28:57 +0500

    Fix detail page images

--- main/models.py
+++ main/models.py
@@ -4,7 +4,7 @@ class Anime(models.Model):
     name = models.CharField(max_length=245)
     izoh = models.CharField(max_length=245)
     janr = models.CharField(max_length=245)
-    yili = models.DateField()
+    yili = models.CharField(max_length=4)
     rasm = models.ImageField(upload_to="anime")
     def __str__(self):
         return self.name
